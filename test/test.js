8fe2b781cefe64190f063a11fd9f16ffde92b57d
Hendri Pretorius <pretorh@gmail.com>
2013-10-13 13:19:22 +0200

    import code test, lib

---
+++ test/test.js
@@ -0,0 +1,98 @@
+var assert = require("assert"),
+    defaultify = require("../");
+
+/*
+ * setup
+*/
+
+var myobject = {
+    test: "hello",
+    notSpecified: 5,
+    x: "a string and not a number",
+    obj: {
+        a: 5
+    },
+    obj2: "a string and not a number"
+};
+
+var defaults = {
+    test: "a",
+    i: 5,
+    x: 6.5,
+    obj: {
+        a: 6,
+        b: 7
+    },
+    arr: [1, 2, 3],
+    obj2: {
+        x: 5,
+        y: 7
+    },
+    func: function() { return 5; }
+};
+
+/*
+ * when
+*/
+
+var result = defaultify(myobject, defaults);
+var values = result.value;
+console.log(result);
+
+/*
+ * then
+*/
+
+function defaultIsReturnedIfNotFound() {
+    assert.equal(values.i, 5);
+}
+
+function userValuesNotSpecifiedInDefaultNotReturned() {
+    assert.equal(values.notSpecified, undefined);
+}
+
+function userValuesNotSpecifiedInDefaultInInfo() {
+    assert.equal(result.info["notSpecified"], "unknown key");
+}
+
+function useUserValueIfPresent() {
+    assert.equal(values.test, "hello");
+}
+
+function newObjectIsReturned() {
+    var user = {};
+    var dflt = {a: 6};
+    var res = defaultify(user, dflt);
+    assert.equal(res.value.a, 6);
+    dflt.a = 7;
+    assert.equal(res.value.a, 6, "value changed after default changed");
+}
+
+function useUserValueOnlyIfSameType() {
+    assert.equal(values.x, 6.5);
+    assert.equal(result.info["x"], "invalid type");
+}
+
+function copiesObjects() {
+    assert.equal(values.obj.a, 5);
+    assert.equal(values.obj.b, 7);
+}
+
+function arraysAreCopied() {
+    assert.equal(values.arr.length, 3);
+}
+
+function subObjectsCopiedCorrectly() {
+    assert.equal(typeof(values.obj2), "object");
+    assert.equal(values.obj2.x, 5);
+}
+
+process.nextTick(defaultIsReturnedIfNotFound);
+process.nextTick(userValuesNotSpecifiedInDefaultNotReturned);
+process.nextTick(userValuesNotSpecifiedInDefaultInInfo);
+process.nextTick(useUserValueIfPresent);
+process.nextTick(newObjectIsReturned);
+process.nextTick(useUserValueOnlyIfSameType);
+process.nextTick(copiesObjects);
+process.nextTick(arraysAreCopied);
+process.nextTick(subObjectsCopiedCorrectly);
